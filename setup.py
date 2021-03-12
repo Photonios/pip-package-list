efe3d696ea3f2579d7f94684d1ad13d24d9da9f5
Swen Kooij <swen@sectorlabs.ro>
2021-03-12 15:02:05 +0200

    Bump version number to 0.0.4

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open(
 
 setup(
     name="pip-package-list",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",
